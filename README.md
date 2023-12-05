cc0091938fd702609034593aa1a943adc82546af
刘永 <liuyong@outlook.com>
2023-12-05 09:38:26 +0800

    第一版本完成

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alien_invasion
+跟着《Python 编程-从入门到实战》中的项目练习
---
+++ settings.py
@@ -0,0 +1,51 @@
+class Settings():
+  """
+  Settings 存储所有的设置类
+  """
+
+  def __init__(self, screen_width = 1200, screen_height = 800, bg_color=(230,230,230)) -> None:
+    # 屏幕设置
+    self.screen_width = screen_width
+    self.screen_height = screen_height
+    self.bg_color = bg_color
+
+    # 飞船设置
+    self.ship_speed_factor = 0.8
+    self.ship_limit = 3
+
+    # 子弹设置
+    self.bullets_speed_factor = 3
+    self.bullets_width = 3
+    self.bullets_height = 15
+    self.bullets_color = 255,0,0
+    self.bullets_allowed = 10
+
+    # 外星人设置
+    self.alien_speed_factor = 1
+    self.fleet_drop_speed = 5
+
+    # 加快节奏
+    self.speedup_scale = 1.1
+    self.score_scale = 1.5
+
+    self.initialize_dynamic_settings()
+
+  def initialize_dynamic_settings(self):
+    self.ship_speed_factor = 1.5
+    self.bullets_speed_factor = 3
+    self.alien_speed_factor = 1
+
+    # 1: 左， -1： 右
+    self.fleet_direction = 1
+
+    # 记分
+    self.alien_points = 50
+
+  def increase_speed(self):
+    """提高速度设置和得分设置"""
+    self.ship_speed_factor *= self.speedup_scale
+    self.bullets_speed_factor *= self.speedup_scale
+    self.alien_speed_factor *= self.speedup_scale
+    self.alien_points = int(self.alien_points * self.score_scale)
+
+  
